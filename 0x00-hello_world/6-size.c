8ff208c1a328f16ac1a68fd0a794725f280fd973
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-16 16:55:32 +0000

    Added the last file 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Should return 0 always
+ */
+int main(void)
+{
+	long long i;
+
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long));
+	printf("Size of a long long int : %d\n", sizeof(i));
+	printf("Size of a float: %d\n", sizeof(float));
+	return (0);
+}
