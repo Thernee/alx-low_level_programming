7109cd197d21f78a82726065aeb184701d865718
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-16 16:08:42 +0000

    Uploaded correct 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char str[100] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
