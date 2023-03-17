b7997119d29b397e3c72eef4b578b78101e797d4
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-17 17:06:59 +0000

    Added the completed 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Here starts the program
+ *
+ * Return: Returns 0 always
+ */
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}
