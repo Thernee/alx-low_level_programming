07713f23edefd63a5f5fe7837fbeb57e50a38531
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-17 20:21:21 +0000

    Reformatted the 9-print_comb.c code

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,16 @@ int main(void)
 {
 	int n = 0;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 10; n++)
 	{
 		putchar('0' + n);
+
+		if (n != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	putchar('0' + n);
+	putchar('\n');
 	return (0);
 }
