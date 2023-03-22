ec4b8dfc376cf83d8e47edc0c1b85c764540532e
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-22 05:32:06 +0000

    First commit, 2nd deadl 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints putchar
+ *
+ * Return: Should return the value _putchar
+ */
+int main(void)
+{
+	printf("\\_putchar\n");
+
+	return (0);
+}
