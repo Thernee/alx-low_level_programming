2187e19cc2cae0c939af034f6f3ef4c1571e3813
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-23 21:50:42 +0000

    Added 0-putchar.c  README.md,  _putchar.c, and  main.h files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - this program prints putchar
+ * main - The program starts here
+ * _putchar - this program prints putchar
  *
  * Return: Should return the value _putchar
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
