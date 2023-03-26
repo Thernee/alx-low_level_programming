662a098aa0aa292455cea9f1d63e96fafbbc375b
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-26 11:41:38 +0000

    Fixed 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,17 +8,15 @@
  */
 int _isupper(int c)
 {
-	for (c = 'A'; c <= 'Z'; c++)
-	{
 		if (c >= 'A' && c <= 'Z')
-		{ 
+		{
 			return (1);
-		
+
 		}
 		else
-		{ 
+		{
 			return (0);
 		}
-	}
+	
 	return (0);
 }
