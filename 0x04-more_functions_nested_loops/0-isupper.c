0f717fe065c3adb9e12c59081c9df17e54ab200e
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-26 10:52:53 +0000

    Added 0-isupper.c, main.h and 0-main.c  _putchar.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for the uppercase character
+ *
+ * @c: The parameter passed to _isupper
+ * Return: returns an uppercase char if successful, 1 if not
+ */
+int _isupper(int c)
+{
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+			_putchar(c);
+			_putchar(':');
+			_putchar(' ');
+			return (1);
+		}
+		else
+		{
+			_putchar(c);
+			_putchar(':');
+			_putchar(' ');
+			return (0);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory contains all my work on the project  0x04. C - More functions, more nested loops
+
