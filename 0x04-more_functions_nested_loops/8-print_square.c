4d55ace4323b430ef9646718f08d2f8adff53a3d
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-26 21:18:49 +0000

    for 8-print_square.c, Inputs below 0 prints newline now

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,19 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 1; i <= size; i++)
+	if (size <= 0)
 	{
-		for (j = 1; j <= size; j++)
+		_putchar('\n');
+	}
+	else if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
 		{
-			if (size > 0)
+			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
