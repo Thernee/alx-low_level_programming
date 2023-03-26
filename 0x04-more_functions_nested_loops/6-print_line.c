f9e8977d1b6a593102045a9f9e0b3ed37010d96b
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-26 20:37:13 +0000

    Added 6-print_line.c, updated main.h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a line based on given number
+ *
+ * @n: Parameter of type int given to print_line
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n > 0)
+		{	_putchar('_');
+
+		}
+
+
+	}
+	_putchar('\n');
+}
