da322faf5269e6af969cacec0d6bf5382f591160
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-28 04:26:16 +0000

    Added 6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints even numbers from 0-9 including 0
+ *
+ * @str: String to be printed from
+ * Return: Returns nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i  = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ int _putchar(char c);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
 
 #endif
 
