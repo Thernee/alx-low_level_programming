3bbf031d17aad9e063ad52a5240ae47685a95887
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-29 20:53:33 +0000

    Added 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,4 +9,31 @@
 void puts_half(char *str)
 {
 	int i;
+	int end;
+	int counter;
+
+	counter = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		counter++;
+	}
+	if (counter % 2 == 0)
+	{
+		end  = counter / 2;
+
+		for (i = end; i <= counter; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		end = (counter - 1) / 2;
+		for (i = end; i <= counter; i++)
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
 }
