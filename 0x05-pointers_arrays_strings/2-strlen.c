0c0e2208c49eb22844badfc665d6d10b93dfebda
Abubakar Adam Sani <Athernee@gmail.com>
2023-03-29 14:25:22 +0000

    Removed putchar from 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ * _strlen - Returns the length of a string
  *
- * @s: char parameter to count
- * Return: returns number of strings
+ * @s: String integer given to the function
+ * Return: Returns number of strings
  */
 int _strlen(char *s)
 {
 	int i;
-	int sum = 1;
+	int sum = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		sum += i;
+		sum++;
 	}
-	_putchar('0' + sum);
-
-	return (0);
+	return (sum);
 }
