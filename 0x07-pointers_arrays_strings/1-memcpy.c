5dc39a4ecacc2f8d7838046a6bf1e57aa4202a95
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-03 17:16:01 +0000

    changed i unsigned for 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i, m;
-	m = n;
+	unsigned int i;
 
-	for (i = 0; i < m; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
