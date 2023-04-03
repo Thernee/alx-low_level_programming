1446636159d5e33e6cd1e7d7b3c9111315662406
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-03 21:47:25 +0000

    Successfully loops through s and accept for 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,18 +9,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, count;
+	int count = 0;
+	char *ptr;
 
-	count = 0;
-	for (i = 0; accept[i] != '\0'; i++)
+	for (; *s; s++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (ptr = accept; *ptr && *ptr != *s; ptr++)
 		{
-			if (accept[i] == s[j])
-			{
-				count++;
-			}
 		}
+		if (!*ptr)
+		{
+			break;
+		}
+		count++;
 	}
 	return (count);
 }
