a328afc91f542ddf0f00aa784e68696b61394456
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-03 17:00:32 +0000

    Corrected loop for 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: A C String
+ * @c: character to be located
+ * Return: Returns a pointer to the first occurence of c or null otherwise
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
