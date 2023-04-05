ef173acaf73765c6d5692994773ffe5d4466df1f
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-05 16:53:57 +0000

    Fixed Extra newline issue

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _print_rev_recursion - prints a string in reverse using recursion
@@ -8,13 +9,14 @@
  */
 void _print_rev_recursion(char *s)
 {
+	if ( s == NULL)
+	{
+		return;
+	}
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	else
-	{
-		_putchar('\n');
-	}
 }
+
