c40a3dae68bc32f5a6eb889f24f2068071544bdd
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-06 22:38:08 +0000

    Fixed helper function's iteration

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * is_prime_H - helper function for is_prime_number
+ *
+ * @x: Given number
+ * @y: Number used to iterate
+ * Return: Returns 1 for prime and 0 otherwise
+ */
+int is_prime_H(int x, int y)
+{
+	if (y <= 1) 
+		return (1);
+
+	if (x % y == 0)
+		return (0);
+
+	return (is_prime_H(x, y - 1)); 
+}
+
+/**
  * is_prime_number - returns 1 if prime and 0 otherwise
  *
  * @n: integer input
@@ -8,12 +26,9 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0)
-	{
+	if (n < 2) 
 		return (0);
-	}
-	else if (n / 2 == 0)
-	{
 
-	}
+	return (is_prime_H(n, n / 2)); 
 }
+
