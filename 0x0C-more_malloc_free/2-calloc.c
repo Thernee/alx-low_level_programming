c97568738d09cdce8b37614e113e99804032a754
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-13 23:40:51 +0000

    "Added 2-calloc.c"

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - Allocates memory of nmemb of size bytes each
+ * @nmemb: members to be allocated
+ * @size: Size of the members in bytes
+ * Return: pointer to allocated block
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (size * nmemb); i++)
+	{
+		ptr[i] = 0;
+	}
+	return (ptr);
+
+}
