999bafef207db1d8ebf0c0966b1dfda841a4442c
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-17 23:47:44 +0000

    "Added 2-print_dog.c"

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - print complete struct of dog
+ *
+ * @d: The structure
+ * Return: Nothing
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	else if (d->owner == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
