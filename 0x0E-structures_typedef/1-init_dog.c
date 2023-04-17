879c01fd6ec18bac713952805246799fbc0387a5
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-17 23:11:43 +0000

    "Allocated space if d is NULL"

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "dog.h"
 
 /**
- * struct init_dog - initializes var of type struct dog
+ * init_dog - initializes var of type struct dog
  *
- *
+ * @name: name of dogs
+ * @age: age of dog
+ * @owner: owner of dog
+ * @d: represent the struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
