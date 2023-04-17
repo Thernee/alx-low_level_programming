b8fd0761b6905f7c0635401b77c464821ba5593c
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-17 22:28:24 +0000

    "Added description for dog.h"

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+/**
+ * struct init_dog - initializes var of type struct dog
+ *
+ *
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Defines a new type
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs owner
+ */
+struct dog
+{
 
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
