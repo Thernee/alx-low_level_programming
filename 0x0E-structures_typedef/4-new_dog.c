fa7bdcd5617cbed859a349f9e77cb289d9070c02
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-18 15:24:41 +0000

    "Added 4-new_dog.c"

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,69 @@
 #include "dog.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+/**
+ * copy_string -copies string from src to dest
+ *
+ * @dest:destination
+ * @src: source
+ * Return: nothing
+ */
+void copy_string(char *dest, const char *src)
+{
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+}
 
 /**
  * new_dog - creates a new dog struct
  *
- * @name: name of dog
+ * @name: name of the dog
  * @age: Age of dog
  * @owner: name of owner
- * Return: Pointer to new dog
+ * Return: copy of new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dogcpy = malloc(sizeof(dog_t));
+	int namecount = 0;
+	size_t ownercount = 0;
 	char *namecpy;
 	char *ownercpy;
-	dog_t *dogcpy;
 
-	dogcpy = malloc(sizeof(dog_t));
 	if (dogcpy == NULL)
-		return (NULL);
-
-	namecpy = malloc(sizeof(dogcpy->name) + 1);
-	if (namecpy == NULL)
 	{
-		free(namecpy);
-		free(dogcpy);
 		return (NULL);
 	}
 
-	ownercpy = malloc(sizeof(dogcpy->owner) + 1);
-	if (ownercpy == NULL)
+	while (name[namecount] != '\0')
 	{
+		namecount++;
+	}
+	while (owner[ownercount] != '\0')
+	{
+		ownercount++;
+	}
+	namecpy = malloc(namecount + 1);
+	ownercpy = malloc(ownercount + 1);
+	if (namecpy == NULL || ownercpy == NULL)
+	{
+		free(dogcpy);
+		free(namecpy);
 		free(ownercpy);
-		free(namecpy);
-		free(dogcpy);
 		return (NULL);
 	}
-	dogcpy->name = name;
+
+	copy_string(namecpy, name);
+	copy_string(ownercpy, owner);
+
+	dogcpy->name = namecpy;
 	dogcpy->age = age;
-	dogcpy->owner = owner;
-	namecpy = dogcpy->name;
-	ownercpy = dogcpy->owner;
+	dogcpy->owner = ownercpy;
 
 	return (dogcpy);
 }
+
