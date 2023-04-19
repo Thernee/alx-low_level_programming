126ddd9ec809a0c5db5c9fef2ffae4f2314e315d
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-19 13:38:21 +0000

    "Added 0-print_name.c"

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using function pointers
+ *
+ * @name: Name to be printed
+ * @f: pointer to printing function
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This contains all my tasks on the 0x0F. C - Function pointers
