31ad9169884960ecb2caaad969e25b0269b39994
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-19 14:55:23 +0000

    "Added 2-int_index.c"

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - prints index for when function doesnt return 0
+ *
+ * @array: Array to be searched
+ * @size: size of the array
+ * @cmp: function to compare values of array
+ * Return: index when cmp doesnt return 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int checker = 0;
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			checker = cmp(array[i]);
+			if (checker != 0)
+			{
+				return (i);
+			}
+			if (!checker)
+				return (-1);
+		}
+	}
+	return (i);
+}
