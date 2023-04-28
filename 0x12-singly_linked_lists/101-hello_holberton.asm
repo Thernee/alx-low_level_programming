b5f264bddbec684be889b31c4a3d6fd8d538870d
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-28 21:39:20 +0000

    completed 101-hello_holberton.asm code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,15 @@ section .text
 global _start
 
 _start:
-	mov edx, len
-	mov ecx, msg
-	mov ebx,1
-	mov eax,4
-	int 0x80
+mov edx, length
+mov ecx, message
+mov ebx,1
+mov eax,4
+int 0x80
+
+mov eax, 1
+int 0x80
+
+section .data
+message db 'Hello, Holberton',0Ah
+length equ $ - message
