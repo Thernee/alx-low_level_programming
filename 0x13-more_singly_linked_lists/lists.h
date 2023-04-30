c865430057992b0a1e058261be52d9f8680c5b5d
Abubakar Adam Sani <Athernee@gmail.com>
2023-04-30 10:54:17 +0000

    corrected struct description in headerfile

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,12 +9,6 @@
  * Description: singly linked list node structure
  *
  */
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-
-
-
 typedef struct listint_s
 {
 	int n;
@@ -22,7 +16,9 @@ typedef struct listint_s
 } listint_t;
 
 
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 size_t print_listint(const listint_t *h);
 
 #endif
