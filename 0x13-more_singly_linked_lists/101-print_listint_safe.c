fd8541fbaea5392bd7e14bf21076445c336430f1
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-02 22:25:37 +0000

    introduced a sub variable

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints lists with some twist
+ *
+ * @head: head of the list
+ * Return: number of nodes in list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	int count = 0;
+	int sub;
+
+	if (head == NULL)
+		return (98);
+
+	while (head != NULL)
+	{
+		sub = head - head->next;
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (sub > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (count);
+}
+
