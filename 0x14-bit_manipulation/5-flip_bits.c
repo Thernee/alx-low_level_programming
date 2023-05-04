77699ea1ff0ba7aa46b3b0e2644b6178891ff71e
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-04 18:25:58 +0000

    corrected holder assignment

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,12 +11,13 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int count = 0;
 	unsigned long int holder;
 
-	holder = n ^m;
+	holder = n ^ m;
 
 	while (holder > 0)
 	{
 		count += (holder & 1);
 		holder >>= 1;
 	}
+
 	return (count);
 }
