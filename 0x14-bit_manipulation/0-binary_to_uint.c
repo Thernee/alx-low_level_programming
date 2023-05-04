7d5213ec1a2bbba9314668174b7a83c22de18351
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-04 15:29:50 +0000

    checks for b!= binary in loop

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,13 +11,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int sum = 0;
 	int power = 1, i;
 
-	if (*b != '0' && *b != '1')
-		return (0);
 	if (b == NULL)
 		return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+
 		sum += power * (b[i] - '0');
 		power *= 2;
 	}
