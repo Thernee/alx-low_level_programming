5e86cc1a534f01c7b4379a06c0d038b7726edd59
Abubakar Adam Sani <Athernee@gmail.com>
2023-05-04 00:46:11 +0000

    changed logic again

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -8,64 +8,40 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *my_node, *holder, *visited[1024];
+	listint_t *behind, *ahead, *holder;
+	int count = 0;
 
-	if (h == NULL)
-		return (count);
+	if (h == NULL || *h == NULL)
+		return (0);
 
-	while (*h != NULL)
+	behind = *h;
+	ahead = (*h)->next;
+
+	while (ahead != NULL && ahead < behind)
 	{
+		holder = ahead->next;
+
+		if (ahead == *h)
+			(*h) = holder;
+
+		free(ahead);
 		count++;
-		my_node = *h;
-		*h = (*h)->next;
-		my_node->next = NULL;
 
-		if (contains_node(visited, my_node))
-		{
-			break;
-		}
-
-		visited[count % 1024] = my_node;
-
-		for (holder = *h; holder != NULL && holder != my_node; holder = holder->next)
-		{
-			if (holder->next == my_node)
-			{
-				holder->next = NULL;
-				if (holder == *h)
-					*h = holder->next;
-				if (my_node != NULL)
-					free(my_node);
-				my_node = NULL;
-				break;
-			}
-		}
-
-		free(my_node);
+		ahead = holder;
 	}
 
+	while (behind != NULL)
+	{
+		holder = behind->next;
+		free(behind);
+		count++;
+
+		if (holder != NULL && holder < behind)
+			break;
+
+		behind = holder;
+	}
+
+	*h = NULL;
 	return (count);
 }
-
-/**
- * contains_node - checks if an array of nodes contains a given node
- *
- * @arr: array of nodes to search
- * @node: node to search for
- * Return: true if the array contains the node, false otherwise
- */
-int contains_node(listint_t **arr, listint_t *node)
-{
-	int i;
-
-	for (i = 0; i < 1024; i++)
-	{
-		if (arr[i] == node)
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-
