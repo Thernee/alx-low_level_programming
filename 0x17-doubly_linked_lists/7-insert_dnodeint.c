64f794c0f7c00f48e63147d1fd38fc1775715cb2
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-16 04:55:00 +0000

    corrected login in loop

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,38 +6,43 @@
  * @h: pointer to the head of the list
  * @idx: the given index
  * @n: Data to put in list
- * Return: Address of new node, NUll if failed
+ * Return: Address of new node, NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *holder, *new_node, *temp;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
-	new_node->n = n;
 
-	while (idx != 1)
+	holder = *h;
+	while (idx != 0)
 	{
-		if (holder->next != NULL)
-		{
-			holder = holder->next;
-			idx--;
-		}
-		else
+		if (holder == NULL)
 			return (NULL);
-	}
-	temp = holder->next;
-	new_node->next = temp;
-	new_node->prev = holder;
-	holder->next = new_node;
-	temp->prev = new_node;
 
-	return (*h);
+		holder = holder->next;
+		idx--;
+	}
+
+	temp = holder->prev;
+	new_node->next = holder;
+	new_node->prev = temp;
+	holder->prev = new_node;
+
+	if (temp != NULL)
+		temp->next = new_node;
+	else
+		*h = new_node;
+
+	return (new_node);
 }
+
