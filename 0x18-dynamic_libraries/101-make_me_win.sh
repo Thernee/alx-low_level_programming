782c4f76f2e38eb7baf53c7898cb50baa6d4e367
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-19 21:45:26 +0000

    updated wget flag from -o to -O

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/Thernee/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmega_mil.so -o /tmp/libmega_mil.so
+wget https://github.com/Thernee/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libmega_mil.so -O /tmp/libmega_mil.so
 export LD_PRELOAD=/tmp/libmega_mil.so
