1ea91e7b82499674efef8017e28e5adda49fa876
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-07 11:57:54 +0000

    Wrote 4-hash_table_get.c to get val. from hashtable

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - sets value for a given hashtable
+ * @ht: The hashtable
+ * @key: Key to determine index to get value in table
+ * Return: value at index when successful, NULL otherwise
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return (current->value);
+		}
+		current = current->next;
+	}
+
+	return (NULL);
+}
