0aa95b43e343a8b88013cb0af691db867d5e1785
Abubakar Adam Sani <Athernee@gmail.com>
2023-07-07 11:47:41 +0000

    cleaned up on failed mem alloc

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,19 +30,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
-
-	if (ht->array[index] == NULL)
 	{
-		ht->array[index] = new_node;
-		return (1);
+		free(new_node);
+		return (0);
 	}
+
+	/*
+	 * if (ht->array[index] == NULL)
+	* {
+	*	ht->array[index] = new_node;
+	*	return (1);
+	* }
+	*/
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
